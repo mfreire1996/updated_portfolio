8fb0ac9829193b56edfc8a55a5b3d4900b4049c8
mfreire1996 <mfreire1996@gmail.com>
2019-01-07 20:28:57 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".type", {
+    strings: ["Welcome to my website!", "Enjoy looking at all my previous works!", "Bienvenidos a mi sitio web!", 
+    "¡Disfruta viendo todos mis trabajos anteriores!"],
+    typeSpeed: 70,
+    backSpeed: 70,
+    loop: true
+  });
