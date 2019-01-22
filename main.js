a19d08f1b67933719817d285f15a40a2f02cc924
mfreire1996 <mfreire1996@gmail.com>
2019-01-22 02:35:14 -0500

    More work almost done

--- main.js
+++ main.js
@@ -1,41 +1,9 @@
 var typed = new Typed(".type", {
-    strings: ["Hello! My name is Marcos Freire", "Enjoy looking at all my previous works!"],
-    typeSpeed: 70,
-    backSpeed: 70,
-    loop: true
-  });
-
-
-
-
-
-
-
-
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  strings: [
+    "Hello! My name is Marcos Freire",
+    "Enjoy looking at all my previous works!"
+  ],
+  typeSpeed: 70,
+  backSpeed: 70,
+  loop: true
+});
