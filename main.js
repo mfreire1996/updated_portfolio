4b99cc393c1a9209b29ae4f9dd791bace671d68b
mfreire1996 <mfreire1996@gmail.com>
2019-02-14 22:56:23 -0500

    fix text and  image

--- main.js
+++ main.js
@@ -1,10 +1,51 @@
-var typed = new Typed(".type", {
-  strings: [
-    "Hello! My name is Marcos Freire",
-    "Enjoy looking at all my previous works!"
-  ],
-  typeSpeed: 70,
-  backSpeed: 70,
-  loop: true
+// var typed = new Typed(".type", {
+//   strings: [
+//     "Hello! My name is Marcos Freire",
+//     "Enjoy looking at all my previous works!"
+//   ],
+//   typeSpeed: 70,
+//   backSpeed: 70,
+//   loop: true
+// });
+
+TweenMax.from(".intro", 1.6, {
+  delay: 0.5,
+  opacity: 0,
+  y: 20, 
+  ease: Expo.easeInOut   
 });
+
+TweenMax.from(".s-one", 1.6, {
+  delay: 0.1,
+  opacity: 0,
+  y: 20, 
+  ease: Expo.easeInOut   
+});
+
+TweenMax.from(".s-two", 1.6, {
+  delay: 0.2,
+  opacity: 0,
+  y: 20, 
+  ease: Expo.easeInOut   
+});
+
+TweenMax.from(".s-three", 1.6, {
+  delay: 0.3,
+  opacity: 0,
+  y: 20, 
+  ease: Expo.easeInOut   
+});
+
+TweenMax.from(".intro", 1.6, {
+  delay: 0.4,
+  opacity: 0,
+  y: 20, 
+  ease: Expo.easeInOut   
+});
+
+
+
+
+
+
 
